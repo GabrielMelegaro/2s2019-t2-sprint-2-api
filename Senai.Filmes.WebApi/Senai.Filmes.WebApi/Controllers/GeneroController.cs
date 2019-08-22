0bb55185a426adc4453cadfefe8e6c3540c11c43
Gabriel Melegaro De Oliveira <47218167829@info.local>
2019-08-22 17:26:40 -0300

    Up

---
+++ Senai.Filmes.WebApi/Senai.Filmes.WebApi/Controllers/GeneroController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.Filmes.WebApi.Domains;
+
+namespace Senai.Filmes.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class GeneroController : ControllerBase
+    {
+        List<GeneroDomain> generos = new List<GeneroDomain>()
+        {
+            new GeneroDomain { IdGenero = 1, Nome = "Terror" },
+            new GeneroDomain { IdGenero = 2, Nome = "Comédia" }
+        };
+
+        [HttpGet]
+        public IEnumerable<GeneroDomain> Get()
+        {
+            return generos;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            GeneroDomain Genero = generos.Find(x => x.IdGenero == id);
+            if (Genero == null)
+            {
+                return NotFound();
+            }
+            return Ok(Genero);
+        }
+        [HttpPost]
+        public IActionResult Cadastrar()
+        {
+            generos.Add(new GeneroDomain { IdGenero = generos.Count + 1, Nome = "Ação" });
+            return Ok(generos);
+        }
+    }
+}
