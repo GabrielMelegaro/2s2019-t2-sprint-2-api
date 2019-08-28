75d0bb73d423dda6187c04b8904b9edaf0c9ff58
Gabriel Melegaro De Oliveira <47218167829@info.local>
2019-08-28 17:32:00 -0300

    diaconfuso

---
+++ Senai.Ekips.WebApi/Senai.Ekips.WebApi/Contexts/EkipsContext.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Senai.Ekips.WebApi.Domains
+{
+    public partial class EkipsContext : DbContext
+    {
+        public EkipsContext()
+        {
+        }
+
+        public EkipsContext(DbContextOptions<EkipsContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Cargos> Cargos { get; set; }
+        public virtual DbSet<Departamentos> Departamentos { get; set; }
+        public virtual DbSet<Funcionarios> Funcionarios { get; set; }
+        public virtual DbSet<Usuario> Usuario { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=.\\SqlExpress; Initial Catalog=T_Ekips;User Id=sa;Pwd=132 ");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cargos>(entity =>
+            {
+                entity.HasKey(e => e.IdCargo);
+
+                entity.Property(e => e.Nome)
+                    .IsRequired()
+                    .HasMaxLength(230)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Situacao)
+                    .IsRequired()
+                    .HasMaxLength(280)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Departamentos>(entity =>
+            {
+                entity.HasKey(e => e.IdDepartamento);
+
+                entity.Property(e => e.Nome)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Funcionarios>(entity =>
+            {
+                entity.HasKey(e => e.IdFuncionario);
+
+                entity.HasIndex(e => e.Cpf)
+                    .HasName("UQ__Funciona__C1F897311214E22A")
+                    .IsUnique();
+
+                entity.Property(e => e.Cpf).HasColumnName("CPF");
+
+                entity.Property(e => e.DataNascimento).HasColumnType("date");
+
+                entity.Property(e => e.Nome)
+                    .IsRequired()
+                    .HasMaxLength(240)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Salario).HasColumnType("money");
+
+                entity.HasOne(d => d.IdCargoNavigation)
+                    .WithMany(p => p.Funcionarios)
+                    .HasForeignKey(d => d.IdCargo)
+                    .HasConstraintName("FK__Funcionar__IdCar__5165187F");
+
+                entity.HasOne(d => d.IdDepartamentoNavigation)
+                    .WithMany(p => p.Funcionarios)
+                    .HasForeignKey(d => d.IdDepartamento)
+                    .HasConstraintName("FK__Funcionar__IdDep__5070F446");
+
+                entity.HasOne(d => d.IdUsuarioNavigation)
+                    .WithMany(p => p.Funcionarios)
+                    .HasForeignKey(d => d.IdUsuario)
+                    .HasConstraintName("FK__Funcionar__IdUsu__52593CB8");
+            });
+
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.HasKey(e => e.IdUsuario);
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(250)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Permissao)
+                    .IsRequired()
+                    .HasMaxLength(190)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Senha)
+                    .IsRequired()
+                    .HasMaxLength(400)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
---
+++ Senai.Ekips.WebApi/Senai.Ekips.WebApi/Controllers/FuncionariosController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.Ekips.WebApi.Domains;
+using Senai.Ekips.WebApi.Repositories;
+
+namespace Senai.Ekips.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class FuncionariosController : ControllerBase
+    {
+        FuncionarioRepository FuncionarioRepository = new FuncionarioRepository();
+        
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return Ok(FuncionarioRepository.Listar());
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Funcionarios funcionario)
+        {
+            try
+            {
+                FuncionarioRepository.Cadastrar(funcionario);
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { mensagem = "Algo de ERRADO não está CERTO" + ex.Message });
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            Funcionarios Funcionario = FuncionarioRepository.BuscarPorId(id);
+            if (Funcionario == null)
+                return NotFound();
+            return Ok(Funcionario);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            FuncionarioRepository.Deletar(id);
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Atualizar(Funcionarios funcionario)
+        {
+            try
+            {
+                Funcionarios FuncionarioBuscado = FuncionarioRepository.BuscarPorId(funcionario.IdFuncionario);
+                if (FuncionarioBuscado == null)
+                    return NotFound();
+                FuncionarioRepository.Atualizar(funcionario);
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { mensagem = "Voce Errou hihihi." });
+            }
+        }
+    }
+}
